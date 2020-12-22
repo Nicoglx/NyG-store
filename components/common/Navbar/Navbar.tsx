fbb894be5c709eb21ae06458ba4134a833e42898
Nicoglx <nicman_86@hotmail.com>
2020-12-22 10:28:34 -0300

    New name

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: FC = () => {
           <div className="flex items-center flex-1">
             <Link href="/">
               <a className={s.logo} aria-label="Logo">
-                <Logo />
+                <h3>NyG Store<h3/>
               </a>
             </Link>
             <nav className="hidden ml-6 space-x-4 lg:block">
